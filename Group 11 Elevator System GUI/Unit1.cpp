5077beed719d4e3741b1c791b8e12ef749bbc960
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-05-06 16:10:02 +0000

    Add files via upload

---
+++ Group 11 Elevator System GUI/Unit1.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+	// Initialize the current floor to the first floor
+	currentFloor = 1;
+
+	// Set the text of Label1 to indicate the current floor
+	Label1->Text = "You are currently on Floor: " + IntToStr(currentFloor);
+}
+
+void __fastcall TForm1::ButtonClick(TObject *Sender)
+{
+	// Get the button that was clicked
+	TButton *button = dynamic_cast<TButton*>(Sender);
+
+	// Parse the number from the button's name (e.g. "Button2" => 2)
+	int floor = StrToInt(button->Name.SubString(7, 1));
+
+	// Update the current floor and Label1 text
+	currentFloor = floor;
+	Label1->Text = "Floor " + IntToStr(currentFloor);
+}
+//---------------------------------------------------------------------------
+
+
+
---
+++ Group 11 Elevator System GUI/Unit1.h
@@ -0,0 +1,35 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Controls.Presentation.hpp>
+
+#include <FMX.Types.hpp>//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TButton *Button2;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TLabel *Label1;
+	TLabel *Label2;
+	void __fastcall ButtonClick(TObject *Sender);
+
+private:	// User declarations
+	int currentFloor; // Keeps track of the current floor of the elevator
+
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
+
